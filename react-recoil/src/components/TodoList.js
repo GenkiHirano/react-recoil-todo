198ba3eefa590bd53c17ef65b54785a333b1d4f1
GenkiHirano <genki86web@gmail.com>
2023-02-20 00:14:11 +0900

    feat: リストのフィルター機能を実装

--- react-recoil/src/components/TodoList.js
+++ react-recoil/src/components/TodoList.js
@@ -1,16 +1,18 @@
 import { useRecoilValue } from "recoil";
-import { todoListState } from "../atom";
 import TodoListStats from "./TodoListStats";
 import TodoItemCreator from "./TodoItemCreator";
 import TodoItem from "./TodoItem";
+import { filteredTodoListState } from "../selector";
+import TodoListFilters from "./TodoListFilters";
 
 function TodoList() {
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
       <h1>RecoilによるTodoアプリ</h1>
       <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
       {todoList.map((item) => (
         <TodoItem key={item.id} item={item} />
