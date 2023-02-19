88be4b6532efb837ae45171e89d943164025ac4e
GenkiHirano <genki86web@gmail.com>
2023-02-19 23:11:05 +0900

    feat: atom, selecter, useRecoilValueを使って画面表示

---
+++ react-recoil/src/components/TodoList.js
@@ -0,0 +1,21 @@
+import { useRecoilValue } from "recoil";
+import { todoListState } from "../atom";
+import { todoListStatsState } from "../selector";
+
+function TodoList() {
+  const todoList = useRecoilValue(todoListState);
+  const totalNum = useRecoilValue(todoListStatsState);
+  return (
+    <>
+      <h1>RecoilによるTodoアプリ</h1>
+      <ul>
+        <li>Todoの登録数:{totalNum}</li>
+      </ul>
+      {todoList.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </>
+  );
+}
+
+export default TodoList;
